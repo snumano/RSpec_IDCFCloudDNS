625da1b4354a7f06108dcebb26f827c3b3b37ad3
Shugo Numano <snumano@idcf.jp>
2016-06-10 23:55:43 +0900

    for firefox 46.0.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,5 +9,8 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
+firefox_path = '/Applications/Firefox46_1.app/Contents/MacOS/firefox-bin'
+Selenium::WebDriver::Firefox::Binary.path = firefox_path
+
 Capybara.default_driver = :selenium
 Capybara.app_host = 'https://dns-console.cloud.egg.jp'
